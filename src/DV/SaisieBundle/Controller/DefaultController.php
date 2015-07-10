d6ebef5840c5c11fca7bea098175a899265cdb67
damien-vialon <damien.vialon@edu.itescia.fr>
2015-07-10 13:31:10 +0200

    Formulaire saisie

--- src/DV/SaisieBundle/Controller/DefaultController.php
+++ src/DV/SaisieBundle/Controller/DefaultController.php
@@ -2,12 +2,39 @@
 
 namespace DV\SaisieBundle\Controller;
 
+use DV\SaisieBundle\Entity\Professeur;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('DVSaisieBundle:Default:index.html.twig');
+        $em = $this->getDoctrine()->getManager();
+        $professeur= new Professeur();
+
+        // Création du formulaire
+        $form = $this->createFormBuilder($professeur)
+            ->add('nom', 'text')
+            ->add('prenom', 'text')
+            ->add('Enregistrer le professeur', 'submit')
+            ->getForm();
+
+        $form->handleRequest($this->getRequest());
+
+//        if ($form->isValid()) {
+//            // fait quelque chose comme sauvegarder la tâche dans la bdd
+//
+//            return $this->redirect($this->generateUrl('/saisie/success'));
+//        }
+
+        return $this->render('DVSaisieBundle:Default:index.html.twig', array(
+            'form' => $form->createView(),
+        ));
+
+    }
+
+    public function enregistrerAction()
+    {
+        return "test ok";
     }
 }
