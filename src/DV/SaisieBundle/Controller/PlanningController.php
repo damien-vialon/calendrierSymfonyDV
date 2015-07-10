52c8951a213ccf1b77bc50cd9f39c6af732869c1
damien-vialon <damien.vialon@edu.itescia.fr>
2015-07-10 16:47:13 +0200

    Ajout de l'affichage des professeurs

--- src/DV/SaisieBundle/Controller/PlanningController.php
+++ src/DV/SaisieBundle/Controller/PlanningController.php
@@ -2,12 +2,21 @@
 
 namespace DV\SaisieBundle\Controller;
 
+use DV\SaisieBundle\Entity\Professeur;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class PlanningController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('DVSaisieBundle:Planning:index.html.twig');
+        $repository = $this
+            ->getDoctrine()
+            ->getManager()
+            ->getRepository('DVSaisieBundle:Professeur');
+
+        $listProfesseurs = $repository->findAll();
+
+        return $this->render('DVSaisieBundle:Planning:index.html.twig', array('listProfesseurs' => $listProfesseurs));
+
     }
 }
